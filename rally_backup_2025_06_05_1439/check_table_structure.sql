a738858397469dd1b6a008f584135456ab1ff95d
Ross Freedman <rossfreedman@RossMBP.local>
2025-06-06 06:14:06 -0500

    Clean up backup directory - remove unnecessary backup files from rally_backup_2025_06_05_1439

--- rally_backup_2025_06_05_1439/check_table_structure.sql
+++
@@ -1,5 +0,0 @@
-\d player_availability;
-
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'player_availability'; 
--- rally_backup_2025_06_05_1439/rally_backup_2025_06_04_1630/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .auth import login_required
-
-__all__ = ['login_required'] 
