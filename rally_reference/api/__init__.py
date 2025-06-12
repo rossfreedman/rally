923139a53dd3cfa5d0b42f777ecd6dc9ad7b0ec4
Ross Freedman <rossfreedman@RossMBP.local>
2025-06-11 22:08:54 -0500

    Remove duplicate rally_reference directory to reduce build size - 42MB reduction for Railway deployment

--- rally_reference/api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-API modules for Rally platform tennis application.
-Contains modular API endpoints for various functionalities.
-"""
-
-from .training_data import training_data_bp
-
-__all__ = ['training_data_bp'] 
